eb012ce4a0eafde3763528728d23191e9e4bf0c8
user9 <user9@mail.ru>
2024-06-24 17:26:58 +0200

    Добавьте файлы проекта.

---
+++ test_678/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test_678
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SortArray_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputBox.Text;
+            string[] numbers = input.Split(' ');
+            double[] array = numbers.Select(double.Parse).ToArray();
+            Array.Sort(array, (a, b) => b.CompareTo(a));
+
+            outputText.Text = "Сортировка: " + string.Join(" ", array);
+        }
+    }
+}
